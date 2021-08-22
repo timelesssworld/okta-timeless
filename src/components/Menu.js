c630800574e56457185d1464eb0143fc473f3edb
Kamil <kamil512@gmail.com>
2021-08-22 14:28:09 +0200

    problem with menu fixed

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,10 +4,10 @@ import { Link } from 'gatsby'
 function Menu() {
     return (
         <ul className='menu__items'>
-            <li onMouseOver={savoirVivreHandleOver} onMouseOut={savoirVivreHandleOut} className='menu__item menu__item--savoir-vivre'>
+            <li className='menu__item menu__item--savoir-vivre'>
                 <LinkMenuComponent linkName='savoir-vivre'/>
             </li>
-            <li onMouseOver={dressCodeHandleOver} onMouseOut={dressCodeHandleOut} className='menu__item menu__item--savoir-vivre'>
+            <li className='menu__item menu__item--savoir-vivre'>
                 <LinkMenuComponent linkName='dress-code'/>
             </li>
             <li className='menu__item'>
@@ -17,19 +17,3 @@ function Menu() {
     )
 }
 export default Menu
-const savoirVivreHandleOver = ()=>{
-    const hiddenEl = document.querySelector('.menu__savoir-vivre--hidden');
-    hiddenEl.style.visibility = 'inherit'
-}
-const savoirVivreHandleOut = ()=>{
-    const hiddenEl = document.querySelector('.menu__savoir-vivre--hidden');
-    hiddenEl.style.visibility = 'hidden'
-}
-const dressCodeHandleOver = ()=>{
-    const hiddenEl = document.querySelector('.menu__dress-code--hidden');
-    hiddenEl.style.visibility = 'inherit'
-}
-const dressCodeHandleOut = ()=>{
-    const hiddenEl = document.querySelector('.menu__dress-code--hidden');
-    hiddenEl.style.visibility = 'hidden'
-}
